c096e55499c4c5a05d0e47f5ee82b1656a90e7d5
TankyWoo <wtq1990@gmail.com>
2013-04-20 03:14:26 +0800

    去掉无用的functions;更新screenshot

--- functions.php
+++ functions.php
@@ -13,30 +13,18 @@ if(function_exists('register_nav_menus')){
 	));
 }
 
-
 function my_page_menu_args($args) {
 	$args['show_home'] = true;
 	return $args;
 }
 add_filter('wp_page_menu_args', 'my_page_menu_args');
 
-
-
-
-
 if(function_exists('register_sidebar_widget')){
 	wp_register_sidebar_widget('wtq', '站点', 'site_info');
 }
 
 function site_info(){include(TEMPLATEPATH.'/siteinfo.php');}
-function tab3(){include(TEMPLATEPATH.'/func/r_tab.php');}
-//function foo(){include(TEMPLATEPATH.'/func/Foo.php');}
-
-	 
-//require_once(TEMPLATEPATH . '/func/Foo.php');
 require_once(TEMPLATEPATH. '/func/siteinfo.php');
-require_once(TEMPLATEPATH. '/func/r_tab.php');
-
 require_once(TEMPLATEPATH . '/func/statistics.php');
 require_once(TEMPLATEPATH . '/func/settings.php');
 require_once(TEMPLATEPATH . '/func/setcolor.php');
